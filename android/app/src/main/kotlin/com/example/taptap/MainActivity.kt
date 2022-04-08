f2c4fe25015b217882215337bfadb86a90174dba
Minyong Oh <dhalsdyd@gmail.com>
2022-04-08 18:27:22 +0900

    Adapt : Getx Structure

---
+++ android/app/src/main/kotlin/com/example/taptap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.taptap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/core/theme/color_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  /// 파란색
+  static const MAINCOLOR = Color(0x00446aec);
+
+  /// 하늘색
+  static const SUBCOLOR = Color(0x00e2eaff);
+
+  /// 검은색
+  static const BLACK = Color(0x001d2232);
+
+  /// 회색
+  static const GRAY = Color(0x009ba0ae);
+}
