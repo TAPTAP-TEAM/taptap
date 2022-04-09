e422e7d7b42927f08d0bba8989c27bc35dedf1c8
Minyong Oh <dhalsdyd@gmail.com>
2022-04-09 13:39:20 +0900

    Refacoring : color class name

--- lib/app/core/theme/color_theme.dart
+++ lib/app/core/theme/color_theme.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-abstract class AppColor {
+abstract class AppColorTheme {
   /// 파란색
-  static const MAINCOLOR = Color(0x00446aec);
+  static const MAINCOLOR = Color(0xff446aec);
 
   /// 하늘색
-  static const SUBCOLOR = Color(0x00e2eaff);
+  static const SUBCOLOR = Color(0xffe2eaff);
 
   /// 검은색
-  static const BLACK = Color(0x001d2232);
+  static const BLACK = Color(0xff1d2232);
 
   /// 회색
-  static const GRAY = Color(0x009ba0ae);
+  static const GRAY = Color(0xff9ba0ae);
 }
