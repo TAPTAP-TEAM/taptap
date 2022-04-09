c5e6d826e3269dd47557b84a604e852246847a7c
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 08:59:30 +0900

    Add : Main text theme

--- lib/app/core/theme/text_theme.dart
+++ lib/app/core/theme/text_theme.dart
@@ -4,9 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:taptap/app/core/theme/color_theme.dart';
 
 abstract class AppTextTheme {
+  /// 보통 
   static const REGULAR = TextStyle(
       fontFamily: "Pretendard",
       fontWeight: FontWeight.bold,
       fontSize: 20,
+      color: AppColorTheme.MAINCOLOR);
+
+  /// 큰
+  static const MAIN = TextStyle(
+      fontFamily: "Pretendard",
+      fontWeight: FontWeight.bold,
+      fontSize: 40,
       color: AppColorTheme.MAINCOLOR);
 }
