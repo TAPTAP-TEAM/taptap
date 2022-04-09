f0a5b88ad8c1d15e45123b20ecda87b54f38246e
Minyong Oh <dhalsdyd@gmail.com>
2022-04-09 13:39:46 +0900

    Add : text_theme

---
+++ lib/app/core/theme/text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:taptap/app/core/theme/color_theme.dart';
+
+abstract class AppTextTheme {
+  static const REGULAR = TextStyle(
+      fontFamily: "Pretendard",
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+      color: AppColorTheme.MAINCOLOR);
+}
