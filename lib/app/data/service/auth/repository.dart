e86525b6bdf51690528cabbecd4bc93992939d1b
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:05:52 +0900

    Change : auth to login func

--- lib/app/data/service/auth/repository.dart
+++ lib/app/data/service/auth/repository.dart
@@ -1,8 +1,9 @@
+import 'package:taptap/app/data/models/issue_token.dart';
 import 'package:taptap/app/data/provider/api.dart';
 
 class AuthRepository {
   final ApiProvider api;
   AuthRepository(this.api);
 
-  login() => api.authorize();
+  login() => api.login();
 }
