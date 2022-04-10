fb28632d92d353e6e97d1e25801161fce080c96e
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:01:40 +0900

    Adpat : Getx Structure

---
+++ lib/app/routes/pages.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:taptap/app/modules/home/binding.dart';
+import 'package:taptap/app/modules/home/page.dart';
+import 'package:taptap/app/modules/init/binding.dart';
+import 'package:taptap/app/modules/login/binding.dart';
+import 'package:taptap/app/modules/login/page.dart';
+import 'package:taptap/app/routes/routes.dart';
+import 'package:taptap/app/modules/init/page.dart';
+
+class AppPages {
+  static final pages = [
+    GetPage(
+        name: Routes.INIT,
+        page: () => const InitPage(),
+        binding: InitPageBinding()),
+    GetPage(
+        name: Routes.LOGIN,
+        page: () => LoginPage(),
+        binding: LoginPageBinding()),
+    GetPage(
+        name: Routes.HOME, page: () => HomePage(), binding: HomePageBinding()),
+  ];
+}
