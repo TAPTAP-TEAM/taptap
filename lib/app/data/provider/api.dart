24de3bea1662da0b00eddf706eabb601d16c6d96
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:00:57 +0900

    Refacoring : simplification

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -3,15 +3,15 @@ import 'package:get/get.dart';
 import 'package:taptap/app/core/utils/headers.dart';
 import 'package:taptap/app/data/models/authorize_param.dart';
 import 'package:taptap/app/data/models/issue_token.dart';
+import 'package:taptap/app/data/models/user.dart';
+import 'package:taptap/app/data/service/auth/service.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 part 'secret_key.dart';
 
 class ApiProvider extends GetConnect {
   final String _baseUrl = "https://testapi.openbanking.or.kr";
-  late AuthorizeResponse authResponse;
-  late IssueTokenResponse issueTokenResponse;
 
-  authorize() async {
+  Future<dynamic> authorize() async {
     WebViewController _webViewController;
 
     final _parameter = AuthorizeParam.regular(CLINET_KEY);
@@ -19,7 +19,7 @@ class ApiProvider extends GetConnect {
     Response response = await get(responseUri);
 
     if (response.statusCode == 200) {
-      Get.dialog(Padding(
+      return Get.dialog(Padding(
         padding: const EdgeInsets.all(16.0),
         child: WebView(
           initialUrl: responseUri,
@@ -27,15 +27,13 @@ class ApiProvider extends GetConnect {
             _webViewController = webViewController;
           },
           javascriptMode: JavascriptMode.unrestricted,
-          onPageFinished: (url) {
-            print("FINISH : $url");
-          },
-          navigationDelegate: (request) {
-            print("NAVI : ${request.url}");
-            if (request.url.contains("https://www.kftc.or.kr/?code")) {
-              authResponse = AuthorizeResponse.uri(request.url);
-              print("FINAL : ${authResponse.code}");
+          onPageFinished: (url) async {
+            if (url.contains("https://www.kftc.or.kr/?code")) {
+              Get.back(result: AuthorizeResponse.uri(url));
+              return;
             }
+          },
+          navigationDelegate: (request) async {
             return NavigationDecision.navigate;
           },
         ),
@@ -43,19 +41,37 @@ class ApiProvider extends GetConnect {
     }
   }
 
-  issueToken() async {
-    if (authResponse.code == "") return;
-    final _parameter =
-        IssueTokenParam.regular(authResponse.code, CLINET_KEY, CLIENT_SECRET);
-    Response response = await post("$_baseUrl/oauth/2.0/token", _parameter,
+  Future<IssueTokenResponse> issueToken(String code) async {
+    final _parameter = IssueTokenParam.regular(code, CLINET_KEY, CLIENT_SECRET);
+
+    Response response = await post(
+        "$_baseUrl/oauth/2.0/token", _parameter.stringify,
+        contentType: 'application/x-www-form-urlencoded',
         headers: HeadersAPI().getHeaders());
+
+    return IssueTokenResponse.fromJson(response.body);
   }
 
-  getUserInfo() async {
-    if (issueTokenResponse.accessToken == "") return;
+  Future<IssueTokenResponse> login() async {
+    final auth = await authorize();
+    final issue = await issueToken(auth.code);
+    return issue;
+
+    await authorize().then((value) async {
+      print(value.code);
+      await issueToken(value.code).then((value) {
+        return value.accessToken;
+      });
+    });
+  }
+
+  Future<UserInfo> getUserInfo() async {
     Response response = await get(
-        "$_baseUrl/v2.0/user/me?user_seq_no=${issueTokenResponse.userSeqNo}",
+        "$_baseUrl/v2.0/user/me?user_seq_no=${Get.find<AuthService>().seq_no}",
         headers: HeadersAPI()
-            .getHeaders(auth: true, token: issueTokenResponse.accessToken));
+            .getHeaders(auth: true, token: Get.find<AuthService>().token));
+
+    print(response.body);
+    return UserInfo.fromJson(response.body);
   }
 }
