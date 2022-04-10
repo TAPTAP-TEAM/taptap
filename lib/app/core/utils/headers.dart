850dea1275ba84e47902568602a16afe472ac5a4
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:06:47 +0900

    Refacotring Header

--- lib/app/core/utils/headers.dart
+++ lib/app/core/utils/headers.dart
@@ -1,9 +1,8 @@
-import 'package:get/get.dart';
-
 class HeadersAPI {
   Map<String, String>? getHeaders({bool auth = false, String? token}) {
     Map<String, String> header = {
-      "Content-Type": "application/x-www-form-urlencoded"
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
     };
 
     if (auth == true) {
