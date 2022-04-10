88fe81a596ac7d9862cf477646a2ca9eeba03ee2
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:03:48 +0900

    Save : User Num additionally

--- lib/app/data/service/auth/service.dart
+++ lib/app/data/service/auth/service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
+import 'package:taptap/app/data/models/issue_token.dart';
+import 'package:taptap/app/routes/routes.dart';
 import 'package:taptap/app/data/service/auth/repository.dart';
 import 'package:taptap/app/data/provider/api.dart';
 
@@ -7,33 +9,47 @@ class AuthService extends GetxService {
   late AuthRepository repository;
   final FlutterSecureStorage _storage = Get.find<FlutterSecureStorage>();
   final _token = ''.obs;
+  final _seq_no = ''.obs;
 
-  final STOARGE_KEY = "accessToken";
+  final TOKEN_KEY = "accessToken";
+  final USER_NO = "seq_no";
 
   String get token => _token.value;
+  String get seq_no => _seq_no.value;
 
-  _setToken(String token) async {
+  _setToken(String token, String seq_no) async {
     _token.value = token;
-    await _storage.write(key: STOARGE_KEY, value: token);
+    _seq_no.value = seq_no;
+    await _storage.write(key: TOKEN_KEY, value: token);
+    await _storage.write(key: USER_NO, value: seq_no);
   }
 
   _removeToken() async {
     _token.value = '';
-    await _storage.delete(key: STOARGE_KEY);
+    await _storage.deleteAll();
+
+    Get.offAllNamed(Routes.LOGIN);
   }
 
-  init() async {
+  Future<AuthService> init() async {
     repository = AuthRepository(ApiProvider());
-    String? tokenInStorage = await _storage.read(key: STOARGE_KEY);
+    String? tokenInStorage = await _storage.read(key: TOKEN_KEY);
+    String? seqNoInStorage = await _storage.read(key: USER_NO);
     if (tokenInStorage != null && tokenInStorage != " ") {
       _token.value = tokenInStorage;
+    }
+    if (seqNoInStorage != null && seqNoInStorage != " ") {
+      _seq_no.value = seqNoInStorage;
     }
     return this;
   }
 
-  login() async {
-    String? token = await repository.login();
-    if (token != null) await _setToken(token);
+  Future<void> login() async {
+    IssueTokenResponse response = await repository.login();
+
+    String? token = response.accessToken;
+    String? _seq_no = response.userSeqNo;
+    if (token != null) await _setToken(token, _seq_no);
   }
 
   logout() async {
