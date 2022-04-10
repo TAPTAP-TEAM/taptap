b4c3b6ebb177b1b902c362e70ff9db4dc50b71e9
Minyong Oh <dhalsdyd@gmail.com>
2022-04-10 09:39:48 +0900

    Func : add issueRefreshToken

--- lib/app/data/models/issue_token.dart
+++ lib/app/data/models/issue_token.dart
@@ -1,20 +1,28 @@
 class IssueTokenParam {
-  final String code;
+  final String? code;
   final String clientId;
   final String clientSecret;
   final String redirectUri;
   final String grantType;
+  final String? refreshToken;
 
   const IssueTokenParam(
-      {required this.code,
+      {this.code,
       required this.clientId,
       required this.clientSecret,
       this.redirectUri = "https://www.kftc.or.kr",
-      this.grantType = "authorization_code"});
+      this.grantType = "authorization_code",
+      this.refreshToken});
   factory IssueTokenParam.regular(String code, String id, String secret) {
     final param =
         IssueTokenParam(code: code, clientId: id, clientSecret: secret);
     return param;
+  }
+
+  factory IssueTokenParam.refresh(
+      String id, String secret, String refreshToken) {
+    return IssueTokenParam(
+        clientId: id, clientSecret: secret, refreshToken: refreshToken);
   }
 
   String get stringify {
@@ -23,6 +31,16 @@ class IssueTokenParam {
     result += "client_secret=$clientSecret&";
     result += "code=$code&";
     result += "redirect_uri=$redirectUri&";
+    result += "grant_type=$grantType";
+    return result;
+  }
+
+  String get refreshStringify {
+    String result = "";
+    result += "client_id=$clientId&";
+    result += "client_secret=$clientSecret&";
+    result += "refresh_token=$refreshToken&";
+    result += "scope=login transfer&";
     result += "grant_type=$grantType";
     return result;
   }
